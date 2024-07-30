62f5d3c04525f4537def8f9b36b7686c58429ef7
Jasdeep Madan <jasdeepm@gmail.com>
2024-07-30 16:37:35 +1000

    refactored to use match

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,22 +1,12 @@
 pub fn reply(message: &str) -> &str {
     println!("----{}----", message);
-    if message.chars().any(|c| c.is_ascii_alphabetic())
-        && message.to_uppercase().eq(message)
-        && message.trim().ends_with("?")
-    {
-        return "Calm down, I know what I'm doing!";
+    match message.trim() {
+        m  if m.chars().any(|c| c.is_ascii_alphabetic())
+            && m.to_uppercase().eq(message)
+            && m.trim().ends_with("?")  => "Calm down, I know what I'm doing!",
+        m if m.trim().ends_with("?") => "Sure.",
+        m if !message.chars().any(|c| c.is_ascii_alphanumeric()) => "Fine. Be that way!",
+        m if message.chars().any(|c| c.is_ascii_alphabetic()) && message.to_uppercase().eq(message) => "Whoa, chill out!",
+        _ => "Whatever."
     }
-    if message.trim().ends_with("?") {
-        return "Sure.";
-    }
-
-    if !message.chars().any(|c| c.is_ascii_alphanumeric()) {
-        return "Fine. Be that way!"
-    }
-
-    if message.chars().any(|c| c.is_ascii_alphabetic()) && message.to_uppercase().eq(message) {
-        return "Whoa, chill out!"
-    }
-
-    "Whatever."
 }
